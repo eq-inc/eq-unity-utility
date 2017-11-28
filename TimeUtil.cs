9e6f947a2a331c733d05337a4adc1fb437184e8a
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-11-28 17:07:07 +0900

    一時保存

---
+++ TimeUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Eq.Unity
+{
+    public class TimeUtil
+    {
+        private static readonly DateTime UnixTimeBase = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        public static double UnixTime()
+        {
+            return (DateTime.Now - UnixTimeBase).TotalSeconds;
+        }
+
+        public static double UnixTime(DateTime targetDateTime)
+        {
+            return (targetDateTime - UnixTimeBase).TotalSeconds;
+        }
+
+        public static long UnixTimeMilliseconds()
+        {
+            return (long)((DateTime.Now - UnixTimeBase).TotalMilliseconds);
+        }
+
+        public static long UnixTimeMilliseconds(DateTime targetDateTime)
+        {
+            return (long)((targetDateTime - UnixTimeBase).TotalMilliseconds);
+        }
+    }
+}
